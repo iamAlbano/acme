c218097abb1a0db679efbda230f62d877beba145
Guilherme Albano <GuiAlbano10@gmail.com>
2022-01-28 11:04:29 -0300

    feat: table filters and icons
    added icons on patients table for update and delete and some table filters

--- src/components/patients/TablePatients.js
+++ src/components/patients/TablePatients.js
@@ -1,23 +1,40 @@
 import * as React from 'react';
 import styles from './TablePatients.module.css'
-import { DataGrid } from '@material-ui/data-grid'
+import { DataGrid, GridToolbar, GridActionsCellItem } from '@mui/x-data-grid';
+import { BsPencil as EditIcon, BsFillTrashFill as DeleteIcon } from 'react-icons/bs'
 
 
 export default function TablePatients( { rows }) {
 
     const columns = [
         { field: 'id', headerName: 'ID', width: 100 },
-        { field: 'name', headerName: 'Nome', width: 180 },
+        { field: 'name', headerName: 'Nome', width: 300 },
         { field: 'cpf', headerName: 'CPF', width: 130 },
         { field: 'gender', headerName: 'Sexo', width: 130 },
-        {
-          field: 'date',
-          headerName: 'Data de nascimento',
-          type: 'date',
-          width: 200,
-        },
-
         { field: 'status', headerName: 'Status', width: 120 },
+
+
+        {
+            field: 'actions',
+            type: 'actions',
+            headerName: 'Ações',
+            width: 100,
+            getActions: (params) => [
+
+            <GridActionsCellItem
+                icon={<EditIcon />}
+                label="Edit"    
+              />,
+
+              <GridActionsCellItem
+                icon={<DeleteIcon />}
+                label="Delete"    
+              />,
+
+              
+            ],
+          },
+    
 
       ];
 
@@ -30,6 +47,9 @@ export default function TablePatients( { rows }) {
                 columns={columns}
                 pageSize={5}
                 rowsPerPageOptions={[5]}
+                components={{
+                    Toolbar: GridToolbar,
+                  }}
             />
             </div>
         </>
